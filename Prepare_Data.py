b19e34249963d875acdcd7c7e06c9a668d2b3ef5
TheMP46 <48450452+TheMP46@users.noreply.github.com>
2019-12-25 12:45:37 +0100

    Update Prepare_Data.py

--- Prepare_Data.py
+++ Prepare_Data.py
@@ -14,26 +14,21 @@ import numpy as np
 
 
 # # Main data (Price, onshore, load)
-data=pd.read_csv("./time_series_30min_singleindex.csv")
+data = pd.read_csv("./time_series_30min_singleindex.csv")
 
-columns=data.columns[data.columns.str.contains('IE_')].to_list()
-
+columns = data.columns[data.columns.str.contains('IE_')].to_list()
 columns.append('utc_timestamp')
-
 columns.append('GB_GBN_load_actual_tso')
 columns.append('GB_GBN_solar_generation_actual')
 columns.append('GB_GBN_wind_generation_actual')
-
-df_complete=data[columns]
-df_complete=df_complete.set_index('utc_timestamp')
-
+df_complete = data[columns]
+df_complete = df_complete.set_index('utc_timestamp')
 del df_complete.index.name
-
-df_complete=df_complete.dropna()
-df_complete.index=pd.to_datetime(df_complete.index)
+df_complete = df_complete.dropna()
+df_complete.index = pd.to_datetime(df_complete.index)
 df_complete.head()
 
-column_names={
+column_names = {
     'IE_load_actual_entsoe_transparency':'load',
     'IE_load_forecast_entsoe_transparency':'load_forecast',
     'IE_wind_onshore_generation_actual':'onshore',
@@ -46,14 +41,13 @@ column_names={
     'GB_GBN_wind_generation_actual': 'wind_gb'
 }
 
-df_complete=df_complete.rename(columns=column_names)
+df_complete = df_complete.rename(columns=column_names)
 
 # without _sem < with _sem
 # ->without _sem must be only ROI
 
 df_complete[['price_da_sem','onshore_sem', 'load_forecast_sem', 'load_sem', 'onshore', 'load_forecast', 'load',  'load_gb', 'wind_gb', 'solar_gb']]
-
-df=df_complete[['price_da_sem','onshore_sem', 'load_forecast_sem', 'load_sem', 'load_gb', 'wind_gb', 'solar_gb']]
+df = df_complete[['price_da_sem','onshore_sem', 'load_forecast_sem', 'load_sem', 'load_gb', 'wind_gb', 'solar_gb']]
 
 df.head()
 
@@ -64,20 +58,15 @@ df.head()
 
 # # Weather Data
 
-weather_data=pd.read_csv("./weather_data.csv")
+weather_data = pd.read_csv("./weather_data.csv")
 
-weather_data_columns=weather_data.columns[weather_data.columns.str.contains('IE')].to_list()
-
+weather_data_columns = weather_data.columns[weather_data.columns.str.contains('IE')].to_list()
 weather_data_columns.append('utc_timestamp')
-
-df_weather_data=weather_data[weather_data_columns]
-
-df_weather_data=df_weather_data.set_index('utc_timestamp')
+df_weather_data = weather_data[weather_data_columns]
+df_weather_data = df_weather_data.set_index('utc_timestamp')
 del df_weather_data.index.name
-df_weather_data=df_weather_data.dropna()
-df_weather_data.index=pd.to_datetime(df_weather_data.index)
-
-df_weather_data.head()
+df_weather_data = df_weather_data.dropna()
+df_weather_data.index = pd.to_datetime(df_weather_data.index)
 
 column_names_weather={
     'IE_temperature':'temp',
@@ -86,19 +75,11 @@ column_names_weather={
 }
 
 
-df_weather_data=df_weather_data.rename(columns=column_names_weather)
+df_weather_data = df_weather_data.rename(columns=column_names_weather)
 
-df_weather_data.tail()
-
-
-df=df.resample("60min").mean()
-
-
+df = df.resample("60min").mean()
 df.index.intersection(df_weather_data.index)
-
-
 df[df.index.isin(df.index.intersection(df_weather_data.index))]
-
 result = pd.concat([df[df.index.isin(df.index.intersection(df_weather_data.index))], df_weather_data[df_weather_data.index.isin(df.index.intersection(df_weather_data.index))]], axis=1, sort=False)
 
 result.to_pickle("./data.pkl")
